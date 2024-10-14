ca095db7aadae301f11485ec3e5d90ef86ad8256
u109755b <u109755b@gmail.com>
2024-10-14 23:10:15 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,8 +23,24 @@ docker ps
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# graphviz
+# python version upgrade to 3.9.7
+sudo yum groupinstall "Development Tools" -y
+sudo yum install gcc openssl-devel bzip2-devel libffi-devel -y
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.9.7/Python-3.9.7.tgz
+sudo tar xzf Python-3.9.7.tgz
+cd Python-3.9.7
+sudo ./configure --enable-optimizations
+sudo make altinstall
+python3.9 --version
+sudo rm /usr/bin/python3
+sudo ln -s /usr/local/bin/python3.9 /usr/bin/python3
+python3 --version
+sudo /usr/local/bin/python3.9 -m ensurepip --upgrade
+sudo /usr/local/bin/python3.9 -m pip install --upgrade pip
+
+# libraries
 sudo yum install graphviz -y
 sudo yum install python-pip -y
-pip install graphviz
-
+pip3 install graphviz
+pip3 install pyyaml
